c055750c00f01f9bcc226d5f006a835ea5157ac6
mininjin <mininjin332@gmail.com>
2024-07-07 13:52:27 +0900

    tool: add rollup

---
+++ rollup.config.mjs
@@ -0,0 +1,22 @@
+import typescript from "@rollup/plugin-typescript";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import { defineConfig } from "rollup";
+
+export default defineConfig({
+	input: "src/index.ts",
+	output: {
+		exports: "named",
+		format: "es",
+		file: "dist/index.mjs",
+		sourcemap: true,
+	},
+	plugins: [
+		commonjs(),
+		nodeResolve({
+			exportConditions: ["browser", "worker"],
+			browser: true,
+		}),
+		typescript(),
+	],
+});
